61f64b9a4c12668a179628a78b999af86af78a70
Carol Cordova <carolcordova7@gmail.com>
2017-04-24 16:59:37 -0700

    top file in routes folder, top file in views

---
+++ routes/top/top.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+var knex = require('../../db/knex');
+
+/* GET ALL  home page. */
+router.get('/', function(req, res, next) {
+  res.render('top/top', { title: 'list of locations`' });
+});
+
+//GET ALL
+router.get('/top/top', function(req, res, next) {
+//   knex.raw(`SELECT * from posts WHERE poster_id = ('${req.params.id}')`)
+   knex.raw(`SELECT * from posts WHERE poster_id = ('${1}')`)
+   .then (function(data){
+      res.render('top/top', {posts: data.rows});
+   });
+});
+
+module.exports = router;
