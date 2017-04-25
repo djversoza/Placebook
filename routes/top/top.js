9dc003575d396626d2b2d1f3b651b2a8b4d3c53e
Carol Cordova <carolcordova7@gmail.com>
2017-04-25 15:30:53 -0700

    updating top page

--- routes/top/top.js
+++ routes/top/top.js
@@ -5,8 +5,9 @@ var knex = require('../../db/knex');
 //GET ALL POSTS
 router.get(`/`,function(req,res,next){
    knex.raw(`SELECT *
-       FROM posts JOIN users ON users.id = posts.poster_id where users.logged_in = true`)
+       FROM posts JOIN users ON users.id = posts.poster_id where users.id = ${req.cookies.id}`)
    .then (function(data){
+      console.log("hi",data.rows)
    res.render('top/top', {
          data: data.rows  })
 
@@ -20,9 +21,38 @@ router.get('/:id/edit', function(req, res, next) {
    knex.raw(`SELECT * from posts WHERE id =
    '${req.params.id}'`)
    .then (function(data){
-      res.render('posts/edit', {posts: data.rows[0]});
+      res.render('top/edit', {posts: data.rows[0]});
    });
-});   
+});
+
+
+//-POST ONE
+router.post('/:id',function(req,res,next){
+  console.log(`${req.body.content}`);
+  knex.raw(`INSERT into posts VALUES (DEFAULT,${req.params.id},'${req.body.content}')`).then(function(){
+    res.redirect('/createpost/create')
+  })
+})
+
+
+//-2 POST
+
+
+//UPDATE ACCOUNT
+//-1 GET
+router.get(`/updateacc/:id`,function(req,res,next){
+  knex.raw(`SELECT * FROM users WHERE logged_in='true'`).then(function(data){
+    res.render('top/edit',{
+      data: data.rows[0]
+    })
+  })
+})
+
+router.post('/updateacc/post/:id',function(req,res,next){
+  knex.raw(`UPDATE users SET name='${req.body.name}', password='${req.body.password}' WHERE id=${req.params.id}`).then(function(){
+    res.redirect('/createpost/create')
+  })
+})
 
 //Logout route - DJ
 router.get('/logout', (req, res, next) =>{
@@ -33,6 +63,15 @@ router.get('/logout', (req, res, next) =>{
   });
 
 });
+
+
+module.exports = router;
+
+
+
+
+
+
 
 
 module.exports = router;
