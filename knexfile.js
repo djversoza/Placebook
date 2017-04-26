8edd10f2872fa8afaf02ef4aff1c9b7e7131784c
DJ Versoza <djversoza@gmail.com>
2017-04-26 16:30:10 -0700

    fixed date bug

--- knexfile.js
+++ knexfile.js
@@ -5,6 +5,10 @@ module.exports = {
   development: {
     client: 'pg',
     connection: 'postgres://localhost/placebook'
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
   }
 
 };
